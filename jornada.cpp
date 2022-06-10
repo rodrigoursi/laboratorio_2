aac1dee76abb2d9c631eaa040773abbb1e1964ba
rodrigoursi <rodrigoursi@gmail.com>
2022-06-10 12:23:10 -0300

    layout medio problemas

---
+++ jornada.cpp
@@ -0,0 +1,80 @@
+#include "jornada.h"
+#include "Empleados.h"
+Jornada::Jornada(FechaHora fecha, Empleados legajo, Empleados apellido,
+                Empleados nombre, FechaHora hsEntradaTeorica,
+                FechaHora horaEnt, FechaHora horaSalida, FechaHora horaTotal,
+                char estado, bool ausente)
+{
+    _fecha=fecha;
+    _legajo=legajo;
+    _apellido=apellido;
+    _nombre=nombre;
+    _hsEntradaTeorica=hsEntradaTeorica;
+    _horaEntrada=horaEnt;
+    _horaSalida=horaSalida;
+    _horaTotal=horaTotal;
+    _estado=estado;
+    _ausente=ausente;
+}
+/// ///GET///
+
+    FechaHora Jornada::getFecha(){return _fecha;}
+    Empleados Jornada::getLegajo(){return _legajo;}
+    Empleados Jornada::getApellido(){return _apellido;}
+    Empleados Jornada::getNombre(){return _nombre;}
+    FechaHora Jornada::getHsTeorica(){return _hsEntradaTeorica;}
+    FechaHora Jornada::getHoraEntrada(){return _horaEntrada;}
+    FechaHora Jornada::getHoraSalida(){return _horaSalida;}
+    Fechahora Jornada::getHoraTotal(){return _horaTotal;}
+    string getEstado(){
+        string estado(_estado);
+        return estado;}
+    bool getAusente(){ return _ausente;}
+
+/// ///SET///
+
+void Jornada::setFecha(FechaHora fecha){_fecha=fecha;}
+void Jornada::setLegajo(Empleados legajo){_legajo=legajo;}
+void Jornada::setApellido(Empleados apellido){_apellido=apellido;}
+void Jornada::getNombre(Empleados nombre){_nombre=nombre;}
+void Jornada::setHsTeorica(FechaHora horaTeorica){_hsEntradaTeorica=horaTeorica;}
+void Jornada::setHoraEntrada(FechaHora entrada){_horaEntrada=entrada;}
+void Jornada::setHoraSalida(FechaHora salida){_horaSalida=salida;}
+void Jornada::setHoraTotal(FechaHora total){_horaTotal=total;}
+void Jornada::setEstado(string estado){strcpy(_estado, estado.c_str());}
+void Jornada::setAusente(bool aus){_ausente=aus;}
+
+/// ///DISCO///
+
+bool Jornada::guardarEnDisco(){
+    FILE *p = fopen("jornada.dat","ab");
+        if(p==NULL){
+            return false;
+        }
+        bool guardo= fwrite(this,sizeof (Jornada),1,p);
+        fclose(p);
+        return guardo;
+}
+
+bool Jornada::guardarEnDisco(int pos){
+    FILE *p = fopen("jornada.dat","rb+");
+        if(p==NULL){
+            return false;
+        }
+        fseek(p,pos*sizeof(Estudiante),SEEK_SET);
+        bool guardo= fwrite(this,sizeof (Jornada),1,p);
+        fclose(p);
+        return guardo;
+}
+
+bool Jornada::leerDeDisco(int pos){
+    FILE *p=fopen("jornada.dat","rb");
+    if(p==NULL){
+        false;
+    }
+    fseek(p,pos*sizeof(jornada),SEEK_SET);
+    bool ok= fread(this,sizeof(Estudiante),1,p);
+    fclose(p);
+    return ok;
+}
+
---
+++ jornada.h
@@ -0,0 +1,59 @@
+#ifndef JORNADA_H_INCLUDED
+#define JORNADA_H_INCLUDED
+
+#include <string>
+#include <cstring>
+#include "fecha.h"
+#include "Empleados.h"
+
+using namespace std;
+
+Class jornada{
+
+private:
+    FechaHora _fecha;
+    Empleados _legajo;
+    Empleados _apellido;
+    Empleados _nombre;
+    FechaHora _hsEntradaTeorica;
+    FechaHora _horaEntrada;
+    FechaHora _horaSalida;
+    FechaHora _horaTotal;
+    char _estado[20];
+    bool _ausente;
+
+public:
+    ///constructor
+    Jornada(FechaHora, Empleados, Empleados, Empleados, FechaHora, FechaHora, FechaHora horaSalida, FechaHora, char, bool);
+    ///
+    ///get
+    FechaHora getFecha();
+    Empleados getLegajo();
+    Empleados getApellido();
+    Empleados getNombre();
+    FechaHora getHsTeorica();
+    FechaHora getHoraEntrada();
+    FechaHora getHoraSalida();
+    Fechahora getHoraTotal();
+    string getEstado();
+    bool getAusente();
+    ///
+    ///set
+    void setFecha(FechaHora);
+    void setLegajo(Empleados);
+    void setApellido(Empleados);
+    void getNombre(Empleados);
+    void setHsTeorica(FechaHora);
+    void setHoraEntrada(FechaHora);
+    void setHoraSalida(FechaHora);
+    void setHoraTotal(FechaHora);
+    void setEstado(string);
+    void setAusente(bool);
+    ///
+    bool guardarEnDisco();
+    bool guardarEnDisco(int);
+    bool leerDeDisco(int pos);
+
+};
+
+#endif // JORNADA_H_INCLUDED
